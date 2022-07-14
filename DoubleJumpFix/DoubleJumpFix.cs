bfe27c3a58d614bb2cefbe62c02827b15dc6fde0
Frank Yang <clazex.y@gmail.com>
2022-07-14 14:00:58 +0800

    feat: first version

--- DoubleJumpFix/DoubleJumpFix.cs
+++ DoubleJumpFix/DoubleJumpFix.cs
@@ -2,6 +2,10 @@ using System.Reflection;
 
 using Modding;
 
+using Mono.Cecil.Cil;
+
+using MonoMod.Cil;
+
 namespace DoubleJumpFix;
 
 public sealed class DoubleJumpFix : Mod {
@@ -10,6 +14,16 @@ public sealed class DoubleJumpFix : Mod {
 		.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
 		.InformationalVersion;
 
-	public override void Initialize() {
+	public override void Initialize() =>
+		IL.HeroController.JumpReleased += Fix;
+
+	private static void Fix(ILContext il) {
+		ILLabel label = new ILCursor(il).GotoNext(MoveType.After, i => i.MatchBlt(out _)).MarkLabel();
+		_ = new ILCursor(il)
+			.GotoNext(MoveType.After, i => i.MatchBleUn(out _))
+			.Emit(OpCodes.Ldarg_0)
+			.Emit(OpCodes.Ldfld, ReflectionHelper.GetFieldInfo(typeof(HeroController), nameof(HeroController.cState)))
+			.Emit(OpCodes.Ldfld, ReflectionHelper.GetFieldInfo(typeof(HeroControllerStates), nameof(HeroControllerStates.doubleJumping)))
+			.Emit(OpCodes.Brtrue_S, label);
 	}
 }
